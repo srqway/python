7332e846b214e813d77f53177a419d62f6faaf7e
hsiehpinghan <hsiehpinghan@localhost>
2018-02-18 00:18:21 +0800

    backup.

---
+++ scipy-python/mind-map/freemind_file/Machine Learning Mathematics.mm
@@ -0,0 +1,46 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1518883180038" ID="ID_264599599" MODIFIED="1518883185068" TEXT="Machine Learning Mathematics">
+<node CREATED="1518883257781" ID="ID_605425336" MODIFIED="1518883259162" POSITION="left" TEXT="Linear Algebra">
+<node CREATED="1518883485391" ID="ID_1631595832" MODIFIED="1518883485923" TEXT="Matrices">
+<node CREATED="1518883501447" ID="ID_658907720" MODIFIED="1518883501931" TEXT="Almost all Machine Learning algorithms use Matrix algebra in one way or another. This is a broad subject, too large to be included here in it&#x2019;s full length. Here&#x2019;s a start: https://en.wikipedia.org/wiki/Matrix_(mathematics)">
+<node CREATED="1518883633140" ID="ID_648985176" MODIFIED="1518883633848" TEXT="Basic Operations: Addition, Multiplication, Transposition"/>
+<node CREATED="1518883649780" ID="ID_269923397" MODIFIED="1518883650256" TEXT="Transformations"/>
+<node CREATED="1518883658596" ID="ID_545810780" MODIFIED="1518883661112" TEXT="Trace, Rank, Determinante, Inverse"/>
+</node>
+</node>
+<node CREATED="1518883681172" ID="ID_368043493" MODIFIED="1518883681863" TEXT="Eigenvectors and Eigenvalues">
+<node CREATED="1518883694451" ID="ID_1313616741" MODIFIED="1518883696303" TEXT="In linear algebra, an eigenvector or characteristic vector of a linear transformation T from a vector space V over a field F into itself is a non-zero vector that does not change its direction when that linear transformation is applied to it.">
+<node CREATED="1518883753146" ID="ID_1942156697" MODIFIED="1518883753886" TEXT="http://setosa.io/ev/eigenvectors-and- eigenvalues/"/>
+</node>
+</node>
+<node CREATED="1518883842160" ID="ID_1339217722" MODIFIED="1518883842644" TEXT="Derivatives Chain Rule">
+<node CREATED="1518883855608" ID="ID_749207649" MODIFIED="1518883856211" TEXT="Rule">
+<node CREATED="1518883867735" ID="ID_780998206" MODIFIED="1518883868219" TEXT="Leibniz Notation"/>
+</node>
+</node>
+<node CREATED="1518883905726" ID="ID_69873575" MODIFIED="1518883906251" TEXT="Jacobian Matrix">
+<node CREATED="1518883919358" ID="ID_610568241" MODIFIED="1518883919874" TEXT="The matrix of all first-order partial derivatives of a vector-valued function. When the matrix is a square matrix, both the matrix and its determinant are referred to as the Jacobian in literature"/>
+</node>
+<node CREATED="1518883986845" ID="ID_168261574" MODIFIED="1518883987265" TEXT="Gradient">
+<node CREATED="1518883996852" ID="ID_983998883" MODIFIED="1518883997440" TEXT="The gradient is a multi-variable generalization of the derivative. The gradient is a vector-valued function, as opposed to a derivative, which is scalar-valued."/>
+</node>
+<node CREATED="1518884041188" ID="ID_377550179" MODIFIED="1518884041679" TEXT="Tensors">
+<node CREATED="1518884049363" ID="ID_28790504" MODIFIED="1518884049783" TEXT="For Machine Learning purposes, a Tensor can be described as a Multidimentional Matrix Matrix. Depending on the dimensions, the Tensor can be a Scalar, a Vector, a Matrix, or a Multidimentional Matrix.">
+<node CREATED="1518884089899" ID="ID_95027744" MODIFIED="1518884091078" TEXT="When measuring the forces applied to an infinitesimal cube, one can store the force values in a multidimensional matrix"/>
+</node>
+</node>
+<node CREATED="1518884148753" ID="ID_816309388" MODIFIED="1518884151157" TEXT="Curse of Dimensionality">
+<node CREATED="1518884207048" ID="ID_1177320604" MODIFIED="1518884213228" TEXT="When the dimensionality increases, the volume of the space increases so fast that the available data become sparse. This sparsity is problematic for any method that requires statistical significance. In order to obtain a statistically sound and reliable result, the amount of data needed to support the result often grows exponentially with the dimensionality."/>
+</node>
+</node>
+<node CREATED="1518883270052" ID="ID_1220154052" MODIFIED="1518883270528" POSITION="left" TEXT="Statistics"/>
+<node CREATED="1518883277308" ID="ID_1900115542" MODIFIED="1518883277664" POSITION="left" TEXT="Optimization"/>
+<node CREATED="1518883284092" ID="ID_785058542" MODIFIED="1518883284648" POSITION="left" TEXT="Optimization"/>
+<node CREATED="1518883427593" ID="ID_1819945025" MODIFIED="1518883428469" POSITION="right" TEXT="Cost/Loss(Min) Objective(Max) Functions"/>
+<node CREATED="1518883436233" ID="ID_997987442" MODIFIED="1518883436748" POSITION="right" TEXT="Probability"/>
+<node CREATED="1518883443048" ID="ID_1895255496" MODIFIED="1518883443540" POSITION="right" TEXT="Distributions"/>
+<node CREATED="1518883453472" ID="ID_410006691" MODIFIED="1518883453820" POSITION="right" TEXT="Information Theory"/>
+<node CREATED="1518883461800" ID="ID_847156906" MODIFIED="1518883462180" POSITION="right" TEXT="Density Estimation"/>
+</node>
+</map>
--- scipy-python/mind-map/freemind_file/Machine Learning Process.mm
+++ scipy-python/mind-map/freemind_file/Machine Learning Process.mm
@@ -49,14 +49,49 @@
 <node CREATED="1518863925332" ID="ID_280084080" MODIFIED="1518863925664" TEXT="Torch"/>
 </node>
 </node>
-<node CREATED="1518862637983" ID="ID_861483855" MODIFIED="1518862638419" POSITION="right" TEXT="Data"/>
-<node CREATED="1518862644319" ID="ID_947695217" MODIFIED="1518862644723" POSITION="right" TEXT="Model"/>
-<node CREATED="1518862655215" ID="ID_366613503" MODIFIED="1518862655611" POSITION="right" TEXT="Cost Function"/>
-<node CREATED="1518862660919" ID="ID_1050979733" MODIFIED="1518862661290" POSITION="right" TEXT="Optimization"/>
-<node CREATED="1518862665848" ID="ID_1177412714" MODIFIED="1518862666154" POSITION="right" TEXT="Tuning"/>
-<node CREATED="1518862672127" ID="ID_976608349" MODIFIED="1518862672531" POSITION="right" TEXT="Results and Benchmarking"/>
-<node CREATED="1518862677598" ID="ID_860289134" MODIFIED="1518862677898" POSITION="right" TEXT="Scaling"/>
-<node CREATED="1518862685142" ID="ID_525943622" MODIFIED="1518862685482" POSITION="right" TEXT="Deployment and Operationalisation"/>
-<node CREATED="1518862691846" ID="ID_1848353849" MODIFIED="1518862692219" POSITION="right" TEXT="Infrastructure"/>
+<node CREATED="1518862637983" ID="ID_861483855" MODIFIED="1518862638419" POSITION="right" TEXT="Data">
+<node CREATED="1518881947376" ID="ID_227579489" MODIFIED="1518881948656" TEXT="Find"/>
+<node CREATED="1518881958216" ID="ID_37629046" MODIFIED="1518881960140" TEXT="Collect"/>
+<node CREATED="1518881972184" ID="ID_1725985178" MODIFIED="1518881974986" TEXT="Explore"/>
+<node CREATED="1518881983232" ID="ID_900687872" MODIFIED="1518881987127" TEXT="Clean Features"/>
+<node CREATED="1518881997519" ID="ID_1740425250" MODIFIED="1518881998419" TEXT="Impute Features"/>
+<node CREATED="1518882015687" ID="ID_866214708" MODIFIED="1518882016307" TEXT="Engineer Features"/>
+<node CREATED="1518882031151" ID="ID_35523700" MODIFIED="1518882031706" TEXT="Select Features"/>
+<node CREATED="1518882041406" ID="ID_613556262" MODIFIED="1518882041906" TEXT="Encode Features"/>
+<node CREATED="1518882049798" ID="ID_1112526228" MODIFIED="1518882050202" TEXT="Build Datasets">
+<node CREATED="1518882057662" ID="ID_751563483" MODIFIED="1518882058122" TEXT="Machine Learning is math. In specific, performing Linear Algebra on Matrices. Our data values must be numeric."/>
+</node>
+</node>
+<node CREATED="1518862644319" ID="ID_947695217" MODIFIED="1518862644723" POSITION="right" TEXT="Model">
+<node CREATED="1518882080269" ID="ID_1378217902" MODIFIED="1518882080713" TEXT="Select Algorithm based on question and data available"/>
+</node>
+<node CREATED="1518862655215" ID="ID_366613503" MODIFIED="1518862655611" POSITION="right" TEXT="Cost Function">
+<node CREATED="1518882100957" ID="ID_8814854" MODIFIED="1518882101465" TEXT="The cost function will provide a measure of how far my algorithm and its parameters are from accurately representing my training data."/>
+<node CREATED="1518882130372" ID="ID_1527630675" MODIFIED="1518882130752" TEXT="Sometimes referred to as Cost or Loss function when the goal is to minimise it, or Objective function when the goal is to maximise it."/>
+</node>
+<node CREATED="1518862660919" ID="ID_1050979733" MODIFIED="1518862661290" POSITION="right" TEXT="Optimization">
+<node CREATED="1518882153380" ID="ID_291148194" MODIFIED="1518882153824" TEXT="Having selected a cost function, we need a method to minimise the Cost function, or maximise the Objective function. Typically this is done by Gradient Descent or Stochastic Gradient Descent."/>
+</node>
+<node CREATED="1518862665848" ID="ID_1177412714" MODIFIED="1518862666154" POSITION="right" TEXT="Tuning">
+<node CREATED="1518882941883" ID="ID_60783745" MODIFIED="1518882942263" TEXT="Different Algorithms have different Hyperparameters, which will affect the algorithms performance. There are multiple methods for Hyperparameter Tuning, such as Grid and Random search."/>
+</node>
+<node CREATED="1518862672127" ID="ID_976608349" MODIFIED="1518862672531" POSITION="right" TEXT="Results and Benchmarking">
+<node CREATED="1518882969651" ID="ID_6735061" MODIFIED="1518882970126" TEXT="Analyse the performance of each algorithms and discuss results."/>
+<node CREATED="1518882986986" ID="ID_625031772" MODIFIED="1518882987422" TEXT="Are the results good enough for production?"/>
+<node CREATED="1518883000514" ID="ID_780815300" MODIFIED="1518883001054" TEXT="Is the ML algorithm training and inference completing in a reasonable timeframe?"/>
+</node>
+<node CREATED="1518862677598" ID="ID_860289134" MODIFIED="1518862677898" POSITION="right" TEXT="Scaling">
+<node CREATED="1518883027721" ID="ID_1086954577" MODIFIED="1518883028085" TEXT="How does my algorithm scale for both training and inference?"/>
+</node>
+<node CREATED="1518862685142" ID="ID_525943622" MODIFIED="1518862685482" POSITION="right" TEXT="Deployment and Operationalisation">
+<node CREATED="1518883043785" ID="ID_1638294965" MODIFIED="1518883044157" TEXT="How can feature manipulation be done for training and inference in real-time?"/>
+<node CREATED="1518883069000" ID="ID_686254319" MODIFIED="1518883069292" TEXT="How to make sure that the algorithm is retrained periodically and deployed into production?"/>
+<node CREATED="1518883084008" ID="ID_143410055" MODIFIED="1518883084652" TEXT="How will the ML algorithms be integrated with other systems?"/>
+</node>
+<node CREATED="1518862691846" ID="ID_1848353849" MODIFIED="1518862692219" POSITION="right" TEXT="Infrastructure">
+<node CREATED="1518883098336" ID="ID_1163898439" MODIFIED="1518883098667" TEXT="Can the infrastructure running the machine learning process scale?"/>
+<node CREATED="1518883120991" ID="ID_1984945541" MODIFIED="1518883121316" TEXT="How is access to the ML algorithm provided? REST API? SDK?"/>
+<node CREATED="1518883138223" ID="ID_1168766931" MODIFIED="1518883139035" TEXT="Is the infrastructure appropriate for the algorithm we are running? CPU&apos;s or GPU&apos;s?"/>
+</node>
 </node>
 </map>
